11fc87b5a8344eaa4e072d1e9301e6332ddd37a7
dfranx <dfranx00@gmail.com>
2020-04-11 16:04:05 +0200

    Fix AltGr issue

--- examples/imgui_impl_sdl.cpp
+++ examples/imgui_impl_sdl.cpp
@@ -133,7 +133,7 @@ bool ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event)
             io.KeysDown[key] = (event->type == SDL_KEYDOWN);
             io.KeyShift = ((SDL_GetModState() & KMOD_SHIFT) != 0);
             io.KeyCtrl = ((SDL_GetModState() & KMOD_CTRL) != 0);
-            io.KeyAlt = ((SDL_GetModState() & KMOD_ALT) != 0);
+            io.KeyAlt = ((SDL_GetModState() & KMOD_LALT) != 0);
 #ifdef _WIN32
             io.KeySuper = false;
 #else
